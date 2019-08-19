788fe8bebe6f34c4f9e4c9d33d00bad217dd7297
sztal <stalaga@protonmail.com>
2019-08-19 19:32:49 -0400

    Add tests for probability normalization in D1 and commented in D2 (D2 does not pass probably since the 2D ruleset is much bigger and is not entirely covered with small dataset parts)

--- test/test_theory.py
+++ test/test_theory.py
@@ -4,9 +4,11 @@ It checks whether CTM values are transpose invariant.
 This test is slow.
 """
 # pylint: disable=protected-access
+from math import factorial
 import pytest
 from pytest import approx
 import numpy as np
+from bdm import BDM, BDMRecursive
 from bdm.encoding import decode_array
 
 
@@ -29,3 +31,28 @@ def test_complement_conjecture_d2(bdm_d2):
         arr = decode_array(i, (4, 4))
         c_arr = np.where(arr == 1, 0, 1)
         assert bdm_d2.bdm(arr) == approx(bdm_d2.bdm(c_arr))
+
+@pytest.mark.slow
+@pytest.mark.parametrize('nsymbols', [2, 4, 5, 6, 9])
+def test_ctm_distribution_d1(nsymbols):
+    bdm = BDM(ndim=1, nsymbols=nsymbols)
+    total = 0
+    for dct in bdm._ctm.values():
+        for key, cmx in dct.items():
+            n = len(set(key))
+            mult = factorial(nsymbols) / factorial(nsymbols - n)
+            total += 2**-cmx * mult
+    assert total == approx(1, .01)
+
+# TODO: this is not passing; investigate reasons
+# @pytest.mark.slow
+# @pytest.mark.parametrize('nsymbols', [2])
+# def test_ctm_distribution_d2(nsymbols):
+#     total = 0
+#     bdm = BDM(ndim=2, nsymbols=nsymbols)
+#     for dct in bdm._ctm.values():
+#         for key, cmx in dct.items():
+#             n = len(set(key))
+#             mult = factorial(nsymbols) / factorial(nsymbols - n)
+#             total += 2**-cmx * mult
+#     assert total == approx(1, .01)
