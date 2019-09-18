41daf0203c75ba0a51c7d1816aedc7a4a4c79f5b
sztal <stalaga@protonmail.com>
2019-09-18 22:20:05 +0200

    Add docstrings to theory tests

--- test/test_theory.py
+++ test/test_theory.py
@@ -14,12 +14,14 @@ from bdm.encoding import decode_array
 
 @pytest.mark.slow
 def test_transpose_conjecture(bdm_d2):
+    """Complexity of matrices should be invariant to transposition."""
     for i in range(2**16):
         arr = decode_array(i, (4, 4))
         assert bdm_d2.bdm(arr) == approx(bdm_d2.bdm(arr.T))
 
 @pytest.mark.slow
 def test_complement_conjecture_d1(bdm_d1):
+    """Complexity should be invariant with respect to permuations of symbols."""
     for i in range(2**12):
         arr = decode_array(i, (12, ))
         c_arr = np.where(arr == 1, 0, 1)
@@ -27,6 +29,7 @@ def test_complement_conjecture_d1(bdm_d1):
 
 @pytest.mark.slow
 def test_complement_conjecture_d2(bdm_d2):
+    """Complexity should be invariant with respect to permuations of symbols."""
     for i in range(2**16):
         arr = decode_array(i, (4, 4))
         c_arr = np.where(arr == 1, 0, 1)
@@ -34,7 +37,23 @@ def test_complement_conjecture_d2(bdm_d2):
 
 @pytest.mark.slow
 @pytest.mark.parametrize('nsymbols', [2, 4, 5, 6, 9])
+@pytest.mark.parametrize('size', [12])
+def test_parts_coverage_d1(nsymbols, size):
+    """Check the fraction of possible parts that have CTM values."""
+    bdm = BDM(ndim=1, nsymbols=nsymbols)
+    expected = sum(nsymbols**i for i in range(1, size+1))
+    total = 0
+    for dct in bdm._ctm.values():
+        for key, _ in dct.items():
+            n = len(set(key))
+            mult = factorial(nsymbols) / factorial(nsymbols - n)
+            total += mult
+    assert total / expected >= .25
+
+@pytest.mark.slow
+@pytest.mark.parametrize('nsymbols', [2, 4, 5, 6, 9])
 def test_ctm_distribution_d1(nsymbols):
+    """Check normalization of CTM distributions."""
     bdm = BDM(ndim=1, nsymbols=nsymbols)
     total = 0
     for dct in bdm._ctm.values():
